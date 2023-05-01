95a70e07bb1bfbbd69ee737417c3f10f99e08913
Md-Azizur-Rahaman <132260733+Md-Azizur-Rahaman@users.noreply.github.com>
2023-05-01 15:28:36 +0600

    Add files via upload

---
+++ Uri-Onlinejudge-Solution-main/Uri-Onlinejudge-Solution-main/1001.java
@@ -0,0 +1,19 @@
+package uri;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b,c;
+		  Scanner scan = new Scanner(System.in);
+		  
+		  a = scan.nextInt(); 
+		  b = scan.nextInt();
+		 
+		System.out.println("X = "+(a+b));
+
+	}
+}
+
